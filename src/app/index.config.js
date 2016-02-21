5bb2647ace4a2113f8765c3a45a42010ffa73bec
Varal7 <varaal@gmail.com>
2016-02-21 23:14:10 +0100

    Initiated relation to API

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,9 +6,24 @@
     .config(config);
 
     /** @ngInject */
-    function config($logProvider) {
+    function config($logProvider, DSProvider, DSHttpAdapterProvider) {
         // Enable log
         $logProvider.debugEnabled(true);
+
+        angular.extend(DSProvider.defaults, {
+            useFilter: true
+        });
+        angular.extend(DSHttpAdapterProvider.defaults, {
+            useFilter: true,
+            default: true,
+            deserialize: function(resourceConfig, data) {
+                console.log(data)
+              return data ? ('data' in data ? (('results' in data.data ? data.data.results : data.data)) : data) : data;
+            },
+            basePath: 'http://127.0.0.1:8000/api',
+            forceTrailingSlash: true
+        });
+
     }
-    
+
 })();
