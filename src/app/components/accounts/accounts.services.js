54c89c3ea9f8712aa817fdbfad8e7f4ecb2f6ba6
Varal7 <varaal@gmail.com>
2016-02-22 00:45:30 +0100

    Added route and link to accounts

---
+++ src/app/components/accounts/accounts.services.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('platalbankKhube.accounts')
+        .factory('Account', Account);
+
+    /** @ngInject */
+    function Account(DS) {
+        return DS.defineResource('account');
+    }
+})();
