f3014d441e8cd2f253ca39bd8f4a02d96325a0ae
Varal7 <varaal@gmail.com>
2016-04-08 22:13:15 +0200

    List accounts

--- src/app/components/users/users.controller.js
+++ src/app/components/users/users.controller.js
@@ -7,9 +7,24 @@
     ;
 
     /** @ngInject */
-    function UsersHomeController(User, users, $log) {
+    function UsersHomeController(User, users, accounts, $log, $uibModal) {
         var vm = this;
 
         vm.users = users;
+        vm.accounts = accounts;
+
+
+        vm.openNegative = function() {
+                var modalInstance = $uibModal.open({
+                    templateUrl: "app/components/users/negative.html",
+                    controller: 'UsersNegativeController',
+                    controllerAs: 'UseNegCtl',
+                    resolve: {
+                        accounts: function(Account) {
+                            return Account.findAll();
+                        }
+                    }
+                });
+        };
     }
 })();
