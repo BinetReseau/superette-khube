1b299098935862db5d2824da60eaec920cd24462
Snoopy <anthonyhtyao@gmail.com>
2016-04-09 02:34:11 +0200

    Comme ça Varal7 est content

--- src/app/components/orders/orders.controller.js
+++ src/app/components/orders/orders.controller.js
@@ -20,21 +20,6 @@
         }, 300);
 
         vm.analyse = function(input) {
-
-            var amount = parseFloat(input);
-            if (!isNaN(amount)) {
-                var debit = {
-                    amount: amount,
-                    type: "debit",
-                    description: "Débiter " + amount.toString() + "€"
-                };
-                var credit = {
-                    amount: amount,
-                    type: "credit",
-                    description: "Créditer " + amount.toString() + "€"
-                };
-                return [debit, credit];
-            }
             return ($filter('filter')(accounts,input));
 
         }
